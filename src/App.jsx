daf2f3bff5d1022898e24a6c0853e9de724b3e2f
Snikdhendu Pramanik <136674597+snikdhendu@users.noreply.github.com>
2023-12-20 12:58:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -102,7 +102,7 @@ const App = () => {
                   />
                 </div>
                 <div>
-                  <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Enter your work Description</label>
+                  <label for="last_name" class="block mb-2 text-sm font-medium text-white dark:text-white">Enter your work Description</label>
                   <input 
                   type="text" id="last_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Future secure er jonne :)" required
                   value={des}
